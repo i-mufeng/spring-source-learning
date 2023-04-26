eb7c921e54426863abfdfa00e15bc4cba0202a2e
MuFeng <2563247404@qq.com>
2023-04-26 13:04:45 +0800

    初始化仓库

---
+++ src/test/java/cn/imufeng/springsourcelearning/SpringSourceLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.imufeng.springsourcelearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSourceLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
